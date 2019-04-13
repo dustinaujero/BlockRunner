bc2e3a003d75777c01cbc5dc04ffeecf5f0e71fd
Dustin A <dustin@aujero.net>
2019-04-13 17:02:19 -0400

    Added animation stop on collision

--- game.js
+++ game.js
@@ -1,117 +1,10 @@
-
-// import SimplexNoise from 'simplex-noise';
-// import * as THREE from 'three';
-
-// document.addEventListener("DOMContentLoaded", () => {
-//     let scene;
-//     let camera;
-//     let renderer;
-//     let simplex;
-//     let plane;
-//     let geometry;
-//     let xZoom;
-//     let yZoom;
-//     let noiseStrength;
-
-//     function setup() {
-//         setupNoise();
-//         setupScene();
-//         setupCamera();
-//         setupRenderer();
-//         setupPlane();
-//         setupLights();
-//         setupEventListeners();
-//     }
-
-//     function setupNoise() {
-//         // By zooming y more than x, we get the
-//         // appearence of flying along a valley
-//         xZoom = 6;
-//         yZoom = 35;
-//         noiseStrength = 1.5;
-//         simplex = new SimplexNoise();
-//     }
-
-//     function setupScene() {
-//         scene = new THREE.Scene();
-//     }
-
-//     function setupCamera() {
-//         let res = window.innerWidth / window.innerHeight;
-//     camera = new THREE.PerspectiveCamera(75, res, 0.1, 1000);
-//     camera.position.x = 0;
-//     camera.position.y = -20;
-//     camera.position.z = 3;
-
-//     let controls = new THREE.OrbitControls(camera);
-//     }
-
-//     function setupRenderer() {
-//         renderer = new THREE.WebGLRenderer({
-//             antialias: true
-//         });
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-//     document.body.appendChild(renderer.domElement);
-//     }
-
-//     function setupPlane() {
-//         let side = 120;
-//     geometry = new THREE.PlaneGeometry(40, 40, side, side);
-//     let material = new THREE.MeshStandardMaterial({
-//         roughness: 0.8,
-//     color: new THREE.Color(0x00c500),
-//     });
-//     plane = new THREE.Mesh(geometry, material);
-//     plane.castShadow = true;
-//     plane.receiveShadow = true;
-
-//     scene.add(plane);
-//     }
-
-//     function setupLights() {
-//         let ambientLight = new THREE.AmbientLight(0x0c0c0c);
-//     scene.add(ambientLight);
-
-//     let spotLight = new THREE.SpotLight(0xcccccc);
-//     spotLight.position.set(-30, 60, 60);
-//     spotLight.castShadow = true;
-//     scene.add(spotLight);
-//     }
-
-//     function setupEventListeners() {
-//         window.addEventListener("resize", onWindowResize);
-//     }
-
-//     function onWindowResize() {
-//         camera.aspect = window.innerWidth / window.innerHeight;
-//     camera.updateProjectionMatrix();
-//     renderer.setSize(window.innerWidth, window.innerHeight);
-//     }
-
-//     function draw() {
-//         requestAnimationFrame(draw);
-//     let offset = Date.now() * 0.0004;
-//     adjustVertices(offset);
-//     adjustCameraPos(offset);
-//     renderer.render(scene, camera);
-//     }
-
-//     function adjustVertices(offset) {
-//     for (let i = 0; i < plane.geometry.vertices.length; i++) {let vertex=plane.geometry.vertices[i]; let x=vertex.x /
-//         xZoom; let y=vertex.y / yZoom; let noise=simplex.noise2D(x, y + offset) * noiseStrength; vertex.z=noise; }
-//         geometry.verticesNeedUpdate=true; geometry.computeVertexNormals(); } function adjustCameraPos(offset) {let
-//         x=camera.position.x / xZoom; let y=camera.position.y / yZoom; let noise=simplex.noise2D(x, y + offset) *
-//     noiseStrength + 1.5; camera.position.z=noise; } setup(); draw();
-
-// });
-
-
 document.addEventListener("DOMContentLoaded", () => {
     var scene, camera, renderer, cube, floor, spotLight;
     var cubeDY, cubeDX;
     var frame;
     var keyboard = {};
     var cubes = [];
+    var game;
     var counter = 0;
     const init = () => {
 
@@ -200,7 +93,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }   
 
     const animate = () => {
-        requestAnimationFrame(animate);
+        game = requestAnimationFrame(animate);
         
         if (cube.position.y < -3) {
             cube.position.y = -2.3;
@@ -252,7 +145,7 @@ document.addEventListener("DOMContentLoaded", () => {
             var ray = new THREE.Raycaster(originPoint, directionVector.clone().normalize());
             var collisionResults = ray.intersectObjects(cubes);
             if (collisionResults.length > 0 && collisionResults[0].distance < directionVector.length())
-                console.log("hit");
+                cancelAnimationFrame(game);
         }
 
         cubes.forEach( cube => {
@@ -291,13 +184,15 @@ document.addEventListener("DOMContentLoaded", () => {
         scene.add(box);
     }
     const renderBar = () => {
-        if (Math.random() >= 0.93) {
+        if (Math.random() >= 0.95) {
             const shape = new THREE.BoxGeometry(10, 1, 1);
             const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
             const box = new THREE.Mesh(shape, material);
             cubes.push(box);
+            const xLevels = [0, -1.5, -2.5];
+            const xSample = xLevels[Math.floor(Math.random() * xLevels.length)];
             const xCord = (Math.random() * 12) - 6;
-            box.position.set(xCord, -1.5, -50);
+            box.position.set(xCord, xSample, -50);
             box.castShadow = true;
             box.receiveShadow = true;
             scene.add(box);
