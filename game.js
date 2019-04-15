8d411913ed4b6e35fa436b5ef9cec2ab2eb8dd09
Dustin A <dustin@aujero.net>
2019-04-15 17:17:19 -0400

    CSS Updates

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ import Game from './components/game';
 document.addEventListener("DOMContentLoaded", () => {
     window.game = new Game();
     window.newGame = () => {
-        document.body.removeChild(document.body.children[4]);
+        document.body.removeChild(document.body.children[3]);
         const game = new Game();
         return game;
     }
