d5fc616dee8549667b4eedf12b1e18fd5f730857
Dustin A <dustin@aujero.net>
2019-04-15 17:08:24 -0400

    Resolved difficulty settings playthrough bug

--- game.js
+++ game.js
@@ -2,5 +2,9 @@ import Game from './components/game';
 
 document.addEventListener("DOMContentLoaded", () => {
     window.game = new Game();
-    
+    window.newGame = () => {
+        document.body.removeChild(document.body.children[4]);
+        const game = new Game();
+        return game;
+    }
 })
